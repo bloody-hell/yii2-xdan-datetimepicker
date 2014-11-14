16913965e0b29f17f4760e096f38542712f9660a
Ivan Poponin <juf.halli@gmail.com>
2014-11-14 14:20:03 +0300

    mask

--- DateTimePicker.php
+++ DateTimePicker.php
@@ -4,6 +4,7 @@ namespace bloody_hell\yii2_xdan_datetimepicker;
 
 use yii\helpers\Html;
 use yii\helpers\Json;
+use yii\widgets\MaskedInput;
 
 class DateTimePicker extends \yii\widgets\InputWidget
 {
@@ -29,6 +30,21 @@ class DateTimePicker extends \yii\widgets\InputWidget
 
     public $renderIcon = '<a href="" class="glyphicon glyphicon-calendar"></a>';
 
+    public $mask;
+
+    public $maskOptions = [];
+
+    public $maskEnabled = true;
+
+    protected function generateMask()
+    {
+        return str_replace(
+            array('d',  'm',  'i',  'H',  's',  'y',  'Y'),
+            array('99', '99', '99', '99', '99', '99', '9999'),
+            $this->format
+        );
+    }
+
     public function init()
     {
         parent::init();
@@ -52,6 +68,25 @@ class DateTimePicker extends \yii\widgets\InputWidget
         ], $this->clientOptions);
     }
 
+    protected function renderInput()
+    {
+        if(!$this->maskEnabled){
+            if ($this->hasModel()) {
+                return Html::activeTextInput($this->model, $this->attribute, $this->options);
+            }
+
+            return Html::textInput($this->name, $this->value, $this->options);
+        }
+
+        return MaskedInput::widget([
+            'mask'      => $this->mask ? : $this->generateMask(),
+            'model'     => $this->hasModel() ? $this->model : null,
+            'attribute' => $this->hasModel() ? $this->attribute : null,
+            'value'     => $this->hasModel() ? null : $this->value,
+            'options'   => $this->options,
+            'clientOptions' => $this->maskOptions,
+        ]);
+    }
 
     public function run()
     {
@@ -59,11 +94,7 @@ class DateTimePicker extends \yii\widgets\InputWidget
 
         DateTimePickerAsset::register($this->view);
 
-        if($this->hasModel()){
-            echo Html::activeTextInput($this->model, $this->attribute, $this->options);
-        } else {
-            echo Html::textInput($this->name, $this->value, $this->options);
-        }
+        echo $this->renderInput();
 
         if($this->renderIcon){
             echo $this->renderIcon;
